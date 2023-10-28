bfd3bdea1c8f6a75464a56a23712b1f6ca8db640
Ronilson Amorim <ronilsonamorim327@gmail.com>
2023-10-28 00:01:40 -0300

    Criação (post) e listagem (get) de repositórios

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -12,10 +13,24 @@ const repositories = [];
 
 app.get("/repositories", (request, response) => {
   // TODO
+
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
   // TODO
+  const { title, url, techs } = request.body;
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
@@ -27,7 +42,7 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  //TODO
 });
 
 module.exports = app;
